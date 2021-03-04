a2eaa492cfbf3e1439d737d32db84b6c657c9b4e
rbeyhum <77794249+rbeyhum@users.noreply.github.com>
2021-03-03 23:49:28 -0500

    Update robo_advisor.py to make the user input dynamic based on stock ticker

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,4 +1,23 @@
-# this is the "app/robo_advisor.py" file
+import requests
+import os
+
+user_ticker = input("Please enter the stock ticker that you want to analyze: ")
+
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "IBM")
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+user_ticker+"&apikey="+ALPHAVANTAGE_API_KEY
+
+
+response = requests.get(request_url)
+print(type(response))
+print(response.status_code)
+print(response.text)
+
+
+
+
+
+
+
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
