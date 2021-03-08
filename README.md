6fa0bfb67e812816c03ad2c01b5f9ee6483ab65d
rbeyhum <77794249+rbeyhum@users.noreply.github.com>
2021-03-07 22:12:44 -0500

    Starting a README.md file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# robo-advisor
+# Robo-Advisor Project
+
+This README.md file explains what the program does and how it should be run from the user's end. 
+
+## Program Functionality 
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -68,36 +68,20 @@ def analysis(prices_dict):
 
 while True:
     user_ticker = input("Please input the stock ticker you would like to analyze: ")
-    #ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "IBM")
-    #request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+user_ticker+"&apikey="+ALPHAVANTAGE_API_KEY
-    #response = requests.get(request_url)
-
-    ## first part of input validation is to make sure that it is not a numeric input since no stock tickers contain numbers, even though input always returns a string OR less than 1 character/more than 5 characters
-
-    #if user_ticker.isnumeric() or len(user_ticker) < 1 or len(user_ticker) > 5:
-    #    print("OOPS, this is an invalid input. Make sure to input a valid stock ticker.")
-    #    exit
-    #    # no HTTP request here
-    #else: 
-    #    ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "IBM")
-    #    request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+user_ticker+"&apikey="+ALPHAVANTAGE_API_KEY
-    #    response = requests.get(request_url)
-    #    parsed_response = json.loads(response.text) 
-
-
+    # data validation (prelim) - if an invalid input then no HTTP request
     if user_ticker.isalpha() or 1<= len(user_ticker) >=5:
         ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "IBM")
         request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+user_ticker+"&apikey="+ALPHAVANTAGE_API_KEY
         response = requests.get(request_url)
         parsed_response = json.loads(response.text) 
         
-
+        # valid input, however the ticker does not exist 
         if list(parsed_response.keys())[0] == "Error Message":
             print("OOPS, the stock ticker you input does not exist. Please try again! ")
             exit
         else:
             analytics = analysis(parsed_response)
-            
+
             break
         
     else: 
@@ -124,29 +108,12 @@ print("HAPPY INVESTING!")
 print("-------------------------")
 
 
+added_bonus = input("Would you like to see an extra analysis for your desired ticker? [y/n]")
 
-# writing onto a cvs file 
-
-
-#print("Run at: "+run_time_date.strftime("%I:%M %p")+" on",run_time_date.strftime("%B %d")+", "+run_time_date.strftime("%Y"))
-
-#print(type(response))
-#print(response.status_code)
-##print(response.text)
-#breakpoint()
-#
-
-#
-#latest_day = parsed_response["Meta Data"]["3. Last Refreshed"]
-##
-##
-#tsd = parsed_response["Time Series (Daily)"]
-#all_dates = list(tsd.keys())
-### now here I must sort the dates that are stored inside the list in case the structure of the data changes
-#sorted_dates = sorted(all_dates, reverse=True)
-#matching_date = sorted_dates[0] #since first item in the list is the most recent date
-##
-##
-#latest_close_price = tsd[matching_date]["4. close"]
+if added_bonus == "y":
+    ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "IBM")
+    request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+user_ticker+"outputsize=full&apikey="+ALPHAVANTAGE_API_KEY
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
 
 
